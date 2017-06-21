32392fbb27b05fa698e7f4d8fae9d7ca5a60ab2a
Prannay Khosla <prannay.khosla@gmail.com>
2017-06-21 06:03:20 +0000

    Updating

--- models/wc2.py
+++ models/wc2.py
@@ -62,7 +62,7 @@ with graph.as_default():
   # Ops and variables pinned to the CPU because of missing GPU implementation
   tweet_char_holder = tf.placeholder(tf.int32, shape=[tweet_batch_size,word_max_len,char_max_len],name="tweet_char_holder")
   tweet_word_holder = tf.placeholder(tf.int32, shape=[tweet_batch_size, word_max_len],name="tweet_word_holder")
-  with tf.device('/cpu:0'):
+  with tf.device('/gpu:0'):
     # Look up embeddings for inputs.
     embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
     char_embeddings = tf.Variable(tf.random_uniform([char_vocabulary_size, embedding_size ],-1.0,1.0))
